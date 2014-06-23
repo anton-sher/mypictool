354753ffa93faa8dd39b2cda587598d94503f815
Anton Sherstiuk <anton.sher@gmail.com>
2014-06-23 19:42:28 +0200

    added a fitInto method and final modifiers

--- src/main/java/ua/antonsher/mypictool/LayoutUtil.java
+++ src/main/java/ua/antonsher/mypictool/LayoutUtil.java
@@ -1,6 +1,7 @@
 package ua.antonsher.mypictool;
 
 import javax.annotation.Nonnull;
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,15 +24,37 @@ public class LayoutUtil {
      * @param tileLength inner segment length.
      * @return list of positions of left end of tiles to place. May be empty.
      */
-    public static @Nonnull List<Integer> getEvenDistributionPositions(int offset, int canvasLength, int tileLength) {
-        int effectiveCanvasLength = canvasLength - offset;
-        int fittingTilesCount = effectiveCanvasLength / tileLength;
-        int distanceBetweenTiles = effectiveCanvasLength % tileLength / (fittingTilesCount + 1);
+    public static @Nonnull List<Integer> getEvenDistributionPositions(final int offset, final int canvasLength, final int tileLength) {
+        final int effectiveCanvasLength = canvasLength - offset;
+        final int fittingTilesCount = effectiveCanvasLength / tileLength;
+        final int distanceBetweenTiles = effectiveCanvasLength % tileLength / (fittingTilesCount + 1);
 
-        List<Integer> positions = new ArrayList<>();
+        final List<Integer> positions = new ArrayList<>();
         for (int i = 0; i < fittingTilesCount; ++i) {
             positions.add(offset + distanceBetweenTiles + (tileLength + distanceBetweenTiles) * i);
         }
         return positions;
+    }
+
+    /**
+     * Calculates a result dimension of an item that is downscaled into the canvas.
+     * @param itemDimension dimension of an item to fit onto the canvas.
+     * @param canvasDimension dimension into which to fit the item.
+     * @param border width of the border to leave when fitting the item.
+     * @return resulting dimensions of the item after the downscaling.
+     */
+    public static @Nonnull Dimension fitInto(@Nonnull final Dimension itemDimension, @Nonnull final Dimension canvasDimension, final int border) {
+        final int canvasWidth = canvasDimension.width - 2 * border;
+        final int canvasHeight = canvasDimension.height - 2 * border;
+
+        if (itemDimension.width <= canvasWidth && itemDimension.height <= canvasHeight) {
+            return itemDimension;
+        }
+
+        final float widthRatio = (float)canvasWidth / itemDimension.width;
+        final float heightRatio = (float)canvasHeight / itemDimension.height;
+        final float ratio = Math.min(widthRatio, heightRatio);
+
+        return new Dimension((int)(itemDimension.width * ratio), (int)(itemDimension.height * ratio));
     }
 }
