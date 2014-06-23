253a40894288c6e3467ae59f750a8b8191c12f7e
Anton Sherstiuk <anton.sher@gmail.com>
2014-06-23 23:50:20 +0200

    Extracted 'create tiled image' action

---
+++ src/main/java/ua/antonsher/mypictool/actions/CreateTiledImage.java
@@ -0,0 +1,58 @@
+package ua.antonsher.mypictool.actions;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ua.antonsher.mypictool.Conversions;
+import ua.antonsher.mypictool.ImageFileWriter;
+import ua.antonsher.mypictool.TiledImageBuilder;
+import ua.antonsher.mypictool.filewriter.JavaxImageFileWriter;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Place multiple copies of an image on the white canvas, tiling it evenly.
+ */
+public class CreateTiledImage {
+    private static final Logger logger = LoggerFactory.getLogger(CreateTiledImage.class);
+    private static final int DPI = 300;
+    private static final Dimension DIMENSION_35x45_MM = new Dimension(35, 45);
+    private static final Dimension DIMENSION_10x15_CM = new Dimension(150, 100);
+    private static final String FILE_EXTENSION_REGEX = "(\\.[^.]+)$";
+    private static final String FILE_NAME_ENDING = "-10x15.jpg";
+    private static final String DATE_FORMAT = "YYYY-MM-dd HH:mm:ss";
+
+    public void createTiledImage(File file) throws IOException {
+        logger.debug("Reading file {}", file);
+
+        final BufferedImage input = ImageIO.read(file);
+        final Dimension canvasDimension = Conversions.mmToPixel(DIMENSION_10x15_CM, DPI);
+        final Dimension tileDimension = Conversions.mmToPixel(DIMENSION_35x45_MM, DPI);
+        logger.debug("Dimensions of canvas: {}, tile: {}", canvasDimension, tileDimension);
+
+        final String headerCaption = currentTimeCaption();
+        logger.debug("Building tiled image with caption '{}'", headerCaption);
+        final TiledImageBuilder imageBuilder = new TiledImageBuilder(canvasDimension, headerCaption, DPI);
+        final BufferedImage image = imageBuilder.build(input, tileDimension);
+
+        final String outputFileName = file.getName().replaceAll(FILE_EXTENSION_REGEX, "") + FILE_NAME_ENDING;
+        final File outputFile = new File(outputFileName);
+
+        logger.info("Writing output file {}", outputFile);
+        final ImageFileWriter imageFileWriter = createImageWriter();
+        imageFileWriter.saveAsJpeg(image, outputFile, DPI);
+    }
+
+    private ImageFileWriter createImageWriter() {
+        return new JavaxImageFileWriter();
+    }
+
+    private String currentTimeCaption() {
+        return new SimpleDateFormat(DATE_FORMAT).format(new Date());
+    }
+}
--- src/main/java/ua/antonsher/mypictool/cli/MyPicTool.java
+++ src/main/java/ua/antonsher/mypictool/cli/MyPicTool.java
@@ -2,21 +2,12 @@ package ua.antonsher.mypictool.cli;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import ua.antonsher.mypictool.Conversions;
-import ua.antonsher.mypictool.ImageFileWriter;
-import ua.antonsher.mypictool.TiledImageBuilder;
-import ua.antonsher.mypictool.filewriter.JavaxImageFileWriter;
+import ua.antonsher.mypictool.actions.CreateTiledImage;
 
-import javax.imageio.ImageIO;
-import java.awt.*;
-import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 /**
- * Main class of the pic tool. Provides a CLI interface for tiling a single picture on a larger canvas.
+ * Main class of the pic tool. Provides a CLI interface for some supported actions.
  */
 public class MyPicTool {
     private static final Logger logger = LoggerFactory.getLogger(MyPicTool.class);
@@ -26,12 +17,13 @@ public class MyPicTool {
             System.out.println("Tiled image creation tool. Places 35x45mm tiles on 150x100 mm canvas.");
             System.out.println("Please specify input image files separated by spaces.");
         }
-        for (String arg : args) {
-            File file = new File(arg);
+        for (final String arg : args) {
+            final File file = new File(arg);
             logger.info("Processing input file {}", file);
             if (file.isFile()) {
                 try {
-                    createTiledImage(file);
+                    final CreateTiledImage createTiledImage = new CreateTiledImage();
+                    createTiledImage.createTiledImage(file);
                     logger.info("Processing input file {} finished", file);
                 } catch (Exception e) {
                     logger.warn("Error processing input file {}", file, e);
@@ -40,32 +32,4 @@ public class MyPicTool {
         }
     }
 
-    private static void createTiledImage(File file) throws IOException {
-        logger.debug("Reading file {}", file);
-        BufferedImage input = ImageIO.read(file);
-        int dpi = 300;
-        Dimension canvasDimension = Conversions.mmToPixel(new Dimension(150, 100), dpi);
-        Dimension tileDimension = Conversions.mmToPixel(new Dimension(35, 45), dpi);
-        logger.debug("Dimensions of canvas: {}, tile: {}", canvasDimension, tileDimension);
-
-        String headerCaption = currentTimeCaption();
-        logger.debug("Building tiled image with caption '{}'", headerCaption);
-        TiledImageBuilder imageBuilder = new TiledImageBuilder(canvasDimension, headerCaption, dpi);
-        BufferedImage image = imageBuilder.build(input, tileDimension);
-
-        String fileExtensionRegex = "(\\.[^.]+)$";
-        String outputFileName = file.getName().replaceAll(fileExtensionRegex, "") + "-10x15.jpg";
-        File outputFile = new File(outputFileName);
-        logger.info("Writing output file {}", outputFile);
-        ImageFileWriter imageFileWriter = createImageWriter();
-        imageFileWriter.saveAsJpeg(image, outputFile, dpi);
-    }
-
-    private static ImageFileWriter createImageWriter() {
-        return new JavaxImageFileWriter();
-    }
-
-    private static String currentTimeCaption() {
-        return new SimpleDateFormat("YYYY-MM-dd HH:mm:ss").format(new Date());
-    }
 }
